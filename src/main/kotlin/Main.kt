074ce27b2270bf57e33a75cef7e61cb59f3da3fa
Akech-ann <akechannatong@gmail.com>
2023-02-09 16:57:47 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main(){
+  printname()
+    remainder()
+    add(76,98,64,98)
+    uniquefact()
+}
+fun printname(){
+    var name = "Ann"
+    println("Hello" + " " + name)
+
+
+}
+fun remainder(){
+    var y = 57
+    var z = 9
+    var sum = y % z
+    println(sum)
+}
+fun add(num1:Int, num2:Int, num3:Int, num4:Int){
+    var sum = num1 + num2 + num3 + num4
+    println(sum)
+}
+fun uniquefact(){
+    var uniquefact = "I am a perfectionist"
+    println(uniquefact)
+
+}
